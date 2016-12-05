4f85ff9c7054a7762ffaa51562c931ba05ede051
Mathieu Mathurin <mathurin05@gmail.com>
2016-12-04 22:35:08 -0500

    player now turn and animation start on on movement

--- Assets/TP2/scripts/Players1Controller.cs
+++ Assets/TP2/scripts/Players1Controller.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Players1Controller : MonoBehaviour {
     Animator animator;
     Rigidbody rigidBody;
+    public float speed = 10f;
     // Use this for initialization
     void Start () {
         animator = GetComponent<Animator>();
@@ -11,7 +12,18 @@ public class Players1Controller : MonoBehaviour {
     }
 	
 	// Update is called once per frame
-	void Update () {
-        animator.SetFloat("Speed", 10f);
+	void Update () {        
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        var movement = new Vector3(-moveX, 0f, -moveZ);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        rigidBody.MovePosition(transform.position + movement);        
+        transform.eulerAngles = new Vector3(0, -Mathf.Atan2(moveX, -moveZ) * 180 / Mathf.PI, 0);
+
+        var movingSpeed = Mathf.Abs(moveX) == 1 || Mathf.Abs(moveZ) == 1 ? 1 : 0;
+
+        animator.SetFloat("Speed", movingSpeed);
     }
 }
