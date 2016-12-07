68552f5045b67e93ad0b7d756c4f624edcaa567f
Mathieu Mathurin <mathurin05@gmail.com>
2016-12-07 16:05:31 -0500

    ajout du script pour fixer bug de creation au meme endroit

---
+++ Assets/TP2/scripts/PlatformCollider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformCollider : MonoBehaviour {
+    private int GROUND = 8;
+
+    void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.layer == GROUND)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
