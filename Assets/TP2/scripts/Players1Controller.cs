3b296dda21928e0f03fcc2cd06293d781cdda925
thepape222 <thepape222@gmail.com>
2016-12-08 10:30:22 -0500

    fix lighting issue

--- Assets/TP2/scripts/Players1Controller.cs
+++ Assets/TP2/scripts/Players1Controller.cs
@@ -22,7 +22,7 @@ public class Players1Controller : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {        
 		
-		if (this.transform.position.y < 2) {
+		if (this.transform.position.y < 3) {
 			SceneManager.LoadScene("MainScene");
 		}
         float moveX = Input.GetAxis("Horizontal");
